33b83bbcd8cb5331c54b7ad10102249dd8dee06b
Juan Patricio Marchetto <juanpatriciomarchetto@gmail.com>
2021-08-03 18:23:22 -0300

    commands fixes

--- src/run-commad/run-install-command.ts
+++ src/run-commad/run-install-command.ts
@@ -4,9 +4,12 @@ export const runInstallCommand = (line: string, record: any[], splited: string[]
     const installOutput = [line];
     const componentIndex = record.findIndex(item => item.name === splited[1]);
     if (componentIndex >= 0) {
-        implicitInstall(record, componentIndex, installOutput);
+        if (record[componentIndex].status === status.INSTALLED) {
+            installOutput.push(`${record[componentIndex].name} is already installed`);
+        }else{
         record[componentIndex].status = status.INSTALLED;
-        installOutput.push(`Installing ${record[componentIndex].name}`);
+        implicitInstall(record, componentIndex, installOutput);
+        installOutput.push(`Installing ${record[componentIndex].name}`);}
     } else {
         record.push({
             name: splited[1],
@@ -21,10 +24,10 @@ export const runInstallCommand = (line: string, record: any[], splited: string[]
 const implicitInstall = (record: any[], componentIndex: number, installOutput: string[]) => {
     for (const dependency in record[componentIndex].dependencies) {
         const found = record.findIndex(item => item.name === record[componentIndex].dependencies[dependency]);
-        if (found >= 0 && record[found].status !== status.INSTALLED) {
-            record[found].status = status.IMPLICITLY_INSTALLED;
+        if (found >= 0 && record[found].status === status.NOT_INSTALLED) {
             installOutput.push(`Installing ${record[found].name}`);
             implicitInstall(record, found, installOutput);
+            record[found].status = status.IMPLICITLY_INSTALLED;
         }
     }
 }
--- src/run-commad/run-remove-command.ts
+++ src/run-commad/run-remove-command.ts
@@ -7,10 +7,24 @@ export const runRemoveCommand = (line: string, record: any[], splited: string[])
         if (!record.some(item => item.dependencies.includes(splited[1]) && item.status !== status.NOT_INSTALLED)) {
             removeOutput.push(`Removing ${splited[1]}`);
             record[found].status = status.NOT_INSTALLED;
-            removeOutput.push(`${splited[1]} is removed`);
+            removeUnusedDependencies(record, removeOutput);
         }else{
             removeOutput.push(`${splited[1]} is still needed`);
         }
     }
     return removeOutput;
 }
+
+
+const implicitRemove = (item: any, record: any[],removeOutput:Array<string>) => {
+     const found = record.findIndex(component => component.name === item.name);
+    if (found >= 0) {
+        record[found].status = status.NOT_INSTALLED;
+        removeUnusedDependencies(record, removeOutput);
+        removeOutput.push(`Removing ${record[found].name}`);
+    } 
+}
+
+const removeUnusedDependencies = (record: any[], removeOutput: Array<string>) =>
+    record.filter(item => item.status === status.IMPLICITLY_INSTALLED && !record.some(component => component.dependencies.includes(item.name) && component.status !== status.NOT_INSTALLED)).map(item => implicitRemove(item, record, removeOutput));
+
