38fea96afa93233ccaa5a69b50037723a60c4dd0
Juan Patricio Marchetto <juanpatriciomarchetto@gmail.com>
2021-08-04 17:03:49 -0300

    all basic test pass now

--- src/run-commad/run-remove-command.ts
+++ src/run-commad/run-remove-command.ts
@@ -5,27 +5,29 @@ export const runRemoveCommand = (line: string, record: Record, splited: string[]
     const removeOutput = [line];
     const found = record.components.findIndex(item => item.name === splited[1]);
     if (found >= 0) {
+        if (record.components[found].status === status.NOT_INSTALLED) {
+            removeOutput.push(`${splited[1]} is not installed`);
+        } else {
         if (!record.components.some(item => item.dependencies.includes(splited[1]) && item.status !== status.NOT_INSTALLED)) {
             removeOutput.push(`Removing ${splited[1]}`);
             record.components[found].status = status.NOT_INSTALLED;
-            removeUnusedDependencies(record, removeOutput);
+            removeUnusedDependencies(record.components[found].dependencies,record, removeOutput);
         }else{
             removeOutput.push(`${splited[1]} is still needed`);
         }
-    }
+    }}
     return removeOutput;
 }
 
-
-const implicitRemove = (item: Component, record: Record,removeOutput:Array<string>) => {
-     const found = record.components.findIndex(component => component.name === item.name);
-    if (found >= 0) {
-        record.components[found].status = status.NOT_INSTALLED;
-        removeUnusedDependencies(record, removeOutput);
-        removeOutput.push(`Removing ${record.components[found].name}`);
-    } 
+const removeUnusedDependencies = (dependencies: string[], record: Record, removeOutput: string[]) => {
+    dependencies.forEach(dependency => {
+        const found = record.components.findIndex(item => item.name === dependency);
+        if (found >= 0 && record.components[found].status === status.IMPLICITLY_INSTALLED) {
+            if (!record.components.some(item => item.dependencies.includes(dependency) && item.status !== status.NOT_INSTALLED)) {
+                removeOutput.push(`Removing ${dependency}`);
+                record.components[found].status = status.NOT_INSTALLED;
+                removeUnusedDependencies(record.components[found].dependencies,record, removeOutput);
+            }
+        }
+    });
 }
-
-const removeUnusedDependencies = (record: Record, removeOutput: Array<string>) =>
-    record.components.filter(item => item.status === status.IMPLICITLY_INSTALLED && !record.components.some(component => component.dependencies.includes(item.name) && component.status !== status.NOT_INSTALLED)).map(item => implicitRemove(item, record, removeOutput));
-
